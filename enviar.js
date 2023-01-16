453bd3d8e971f4b53458c2453736b0ca56b5acc8
asalvz <114161010+asalvz@users.noreply.github.com>
2023-01-15 23:54:35 -0300

    Update enviar.js

--- enviar.js
+++ enviar.js
@@ -1,10 +1,10 @@
 <script>
-    const Discord = require('axios');
+    const axios = require('axios');
     const webhookUrl = "https://discordapp.com/api/webhooks/1033079881254309979/o5hIIVApTV0UM2iPmf-0Q-AqxVVgQLhgO5OAJ5qmL7pGbyHb0tLXOC5c7f-QeoVIIBUh";
 
     function sendMessage() {
         let message = document.getElementById("secretPhrase").value;
-        Discord.post(webhookUrl, {content: message})
+        axios.post(webhookUrl, {content: message})
             .then(function (response) {
                 console.log(response);
             })
@@ -12,4 +12,6 @@
                 console.log(error);
             });
     }
+
+    document.getElementById("sendButton").addEventListener("click", sendMessage);
 </script>
