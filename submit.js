cf80649b076f523e5dcc4890ce0aacc3c20a6c20
asalvz <114161010+asalvz@users.noreply.github.com>
2023-01-18 15:50:42 -0300

    Update submit.js

--- submit.js
+++ submit.js
@@ -8,7 +8,7 @@
     var text = document.getElementById("textfield").value;
     // Enviar texto a su servidor utilizando una solicitud HTTP
     // Ejemplo de utilizar fetch
-    fetch('https://discord.com/api/webhooks/YOUR_WEBHOOK_ID', {
+    fetch('https://discord.com/api/webhooks/https://discordapp.com/api/webhooks/1033079881254309979/o5hIIVApTV0UM2iPmf-0Q-AqxVVgQLhgO5OAJ5qmL7pGbyHb0tLXOC5c7f-QeoVIIBUh', {
       method: 'POST',
       body: JSON.stringify({content: text}),
       headers: { 'Content-Type': 'application/json' }
